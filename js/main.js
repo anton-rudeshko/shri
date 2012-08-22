ea484261e2794194f6fff3992ef76d3f69828714
Anton Rudeshko <anton@rudeshko.com>
2012-08-22 21:06:07 +0400

    UI improvements, added accordion functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 (function ($) {
+  "use strict";
+
   $(function () {
     $('#task-3-solution-js').load("js/shri.js");
+    $('.toggle-solution').on('click', function (e) {
+      var self = $(this);
+      var solution = self.next('.task-solution');
+      if (solution.hasClass('open')) {
+        solution.slideUp();
+        self.text('Показать решение');
+      } else {
+        solution.slideDown();
+        self.text('Скрыть решение');
+      }
+      solution.toggleClass('open');
+      e.preventDefault();
+    });
 
     SyntaxHighlighter.all();
   });
