d5790fdc0f8a86c7bbf69614067f24a18927ee33
Anton Rudeshko <anton@rudeshko.com>
2012-08-28 23:39:57 +0400

    Added printable version

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
 
   $(function () {
     $('#task-3-solution-js').load("js/shri.js");
-    $('.toggle-solution').on('click', function (e) {
+    $('.toggle-solution').on('click', function () {
       var self = $(this);
       var solution = self.siblings('.task-solution');
-      if (solution.hasClass('open')) {
+      var isOpened = solution.hasClass('open');
+      if (isOpened) {
         solution.slideUp();
         self.text(self.data('open') || 'Показать решение');
       } else {
@@ -14,8 +15,13 @@
         $('html, body').animate({scrollTop: solution.offset().top}, 1000);
         self.text(self.data('close') || 'Скрыть решение');
       }
-      solution.toggleClass('open');
-      e.preventDefault();
+      solution.toggleClass('open', isOpened);
+      return false;
+    });
+
+    $('#print').on('click', function () {
+      window.print();
+      return false;
     });
 
     SyntaxHighlighter.all();
